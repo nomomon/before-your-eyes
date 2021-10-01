387b617aea329ee98bb1549d816085357e4e6fa9
Mansur <funisinfinity26@gmail.com>
2021-10-01 15:30:39 +0200

    📷 camera turn on/off

---
+++ script.js
@@ -0,0 +1,27 @@
+const startCameraStream = () => {
+    let video = document.querySelector("#video")
+    
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+            });
+    }
+}
+
+const endCameraStream = () => {
+    let video = document.querySelector("#video")
+
+    let stream = video.srcObject;
+    let tracks = stream.getTracks();
+
+    for (let i = 0; i < tracks.length; i++) {
+        let track = tracks[i];
+        track.stop();
+    }
+
+    video.srcObject = null;
+}
