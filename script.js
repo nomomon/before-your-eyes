e033c74db32600ef8fc75e3770a2bf34de6302b4
Mansur <funisinfinity26@gmail.com>
2021-10-01 15:10:57 +0200

    copy camera code

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+async function setupCamera() {
+    video = document.getElementById("video");
+  
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "user",
+        // Only setting the video to a specified size in order to accommodate a
+        // point cloud, so on mobile devices accept the default size.
+        width: mobile ? undefined : VIDEO_SIZE,
+        height: mobile ? undefined : VIDEO_SIZE
+      }
+    });
+    video.srcObject = stream;
+  
+    return new Promise(resolve => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+  }
+
 async function main() {
     // Load the MediaPipe Facemesh package.
     const model = await faceLandmarksDetection.load(
